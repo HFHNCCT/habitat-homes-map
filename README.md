6fe33ef436114ebd9c76e6dfbaff2f94fc4ec07e
Jack Dougherty <jack.dougherty@trincoll.edu>
2021-11-18 10:17:43 -0500

    updated Google Sheet links

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # habitat-homes-map
 Habitat homes in Hartford and Tolland County
+
+## Live map
+https://jackdougherty.github.io/habitat-homes-map/
+
+## Google Sheet data
+https://docs.google.com/spreadsheets/d/18Cq3M9PDK-E6E5eYqEZa-3MfvdNIAX0qoteQvA1gcNk/edit#gid=0
--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bW9xx1WGjvEKF6SkN-N0qqQeBiz2-2RRDUwf3uxttAU/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/18Cq3M9PDK-E6E5eYqEZa-3MfvdNIAX0qoteQvA1gcNk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
